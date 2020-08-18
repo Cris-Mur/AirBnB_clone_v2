2128de7829e7e4a7bbf202d4cbd3107cfa93dc38
Cris-Mur <yomy98@gmail.com>
2020-08-18 04:50:52 +0000

    [change some things]

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,17 +6,16 @@ sudo apt-get -y upgrade
 # Install web server
 sudo apt-get -y install nginx
 # Create Dirs
-sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir /data/web_static/shared/
 # fake index.html
-sudo touch /data/web_static/releases/test/index.html
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 # simbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # user and group
-sudo chown -R ubuntu:ubuntu /data
+sudo chown -R ubuntu:ubuntu /data/
 # Nginx setup
-THING='\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n'
-sudo sed -i "33 a $THING" /etc/nginx/sites-available/default
+THING='location /hbnb_static/ {\n alias /data/web_static/current; \n}\n'
+sudo sed -i "33i $THING" /etc/nginx/sites-available/default
 sudo nginx -t
 sudo service nginx restart
