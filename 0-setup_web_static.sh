b74335d76138267f1d0a41a74d356f06b9cc08c4
Cris-Mur <yomy98@gmail.com>
2020-08-18 00:45:26 +0000

    [fix permissions in the script]

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,13 +6,13 @@ sudo apt-get upgrade -y
 # Install web server
 sudo apt-get install nginx
 # Create Dirs
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 # fake index.html
-touch /data/web_static/releases/test/index.html
-echo "Holberton School" > /data/web_static/releases/test/index.html
+sudo touch /data/web_static/releases/test/index.html
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 # simbolic link
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # user and group
 sudo chown -R ubuntu:ubuntu /data
 # Nginx setup
