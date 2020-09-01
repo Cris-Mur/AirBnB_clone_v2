dd2ff88ef3dd13327e4dda4891cbac3c0c8c28eb
Cris-Mur <yomy98@gmail.com>
2020-09-01 23:23:07 +0000

    [in web-01 server works]

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,11 +9,11 @@ sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir /data/web_static/shared
 # fake index.html
-echo "Holberton School" > /data/web_static/releases/test/index.html
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 # simbolic link
-ln -sf /data/web_static/releases/test /data/web_static/current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 # user and group
 sudo chown -R ubuntu:ubuntu /data/
 # Nginx setup
-sudo sed -i "38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}\n" /etc/nginx/sites-available/default
+sudo sed -i "38i\ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}\n" /etc/nginx/sites-available/default
 sudo service nginx start
