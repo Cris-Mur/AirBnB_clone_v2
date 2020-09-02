6de274782089bd5e1e9e7df8d243381b103a24fb
Cris-Mur <yomy98@gmail.com>
2020-09-02 00:44:24 +0000

    [try to one.. yo]

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import os
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    now_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_bk = "versions/web_static_{:s}.tgz".format(now_time)
+    local("mkdir -p versions")
+    local("tar -cvzf {:s} web_static".format(file_bk))
+    if os.path.exist(file_bk):
+        return file_bk
+    else:
+        return None
